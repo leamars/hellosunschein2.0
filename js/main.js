80e8b1033ac2a1bb9a5f19c5ee2be423270de20e
leamars <maroltso@grinnell.edu>
2014-10-26 00:55:39 -0500

    Main functionality accomplished. Content awaits.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $(".head").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+    });
+
+	});
+
+	$('.bod').waypoint(function() {
+  		$(".socialmedia").fadeIn().css("display", "block");
+      	$(".socialmedia").css("text-align", "center");
+	});
+
+});
