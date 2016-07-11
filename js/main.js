c078618c909236f82439badac5e69e034a82e3f8
leamars <lmarolt@renttherunway.com>
2016-07-10 20:31:09 -0400

    before bootstrap safety check

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+/**********************
+
+Nav Bar Animations
+
+**********************/
+
+var  topBar = $(".main-nav");
+    topBarTop = topBar.position().top;
+    
+$(window).scroll(function() {  
+  if( $(this).scrollTop() > topBarTop ) {
+    topBar.css({
+        "position": "fixed",
+        "top":"0px",
+    });
+      topBar.animate({
+        // backgroundColor: "rgba(255, 255, 255, 0.8)",
+      }, 1000, function() {
+        // Animation complete.
+      });
+  } else {
+    topBar.css({
+        "position": "relative",
+    });
+  }
+});
+
+/**********************
+
+Content Box Toggles
+
+**********************/
+
 $(document).ready(function() {
 
     $(".head").click(function () {
