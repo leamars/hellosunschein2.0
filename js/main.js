9b129445ac1ace8d8d1673ed98d2495c836eb491
leamars <lmarolt@renttherunway.com>
2016-07-16 10:16:28 -0400

    current status

--- js/main.js
+++ js/main.js
@@ -4,25 +4,40 @@ Nav Bar Animations
 
 **********************/
 
-var  topBar = $(".main-nav");
-    topBarTop = topBar.position().top;
+// var  topBar = $(".main-nav");
+//     topBarTop = topBar.position().top;
     
-$(window).scroll(function() {  
-  if( $(this).scrollTop() > topBarTop ) {
-    topBar.css({
-        "position": "fixed",
-        "top":"0px",
+// $(window).scroll(function() {  
+//   if( $(this).scrollTop() > topBarTop ) {
+//     topBar.css({
+//         "position": "fixed",
+//         "top":"0px",
+//     });
+//       topBar.animate({
+//         // backgroundColor: "rgba(255, 255, 255, 0.8)",
+//       }, 1000, function() {
+//         // Animation complete.
+//       });
+//   } else {
+//     topBar.css({
+//         "position": "relative",
+//     });
+//   }
+// });
+
+$( window ).resize(function() {
+    var box = $(".leaBox");
+    console.log($(this).width());
+
+    var boxSize = $(this).width();
+    console.log(boxSize);
+    if (boxSize > 850) {
+      boxSize = $(this).width()/5;
+    }
+    box.css({
+      "width": boxSize + "px",
+      "height": boxSize + "px"
     });
-      topBar.animate({
-        // backgroundColor: "rgba(255, 255, 255, 0.8)",
-      }, 1000, function() {
-        // Animation complete.
-      });
-  } else {
-    topBar.css({
-        "position": "relative",
-    });
-  }
 });
 
 /**********************
@@ -45,5 +60,28 @@ $(document).ready(function() {
     });
 
 	});
+
+    var box = $(".leaBox");
+    var width = $(window).width();
+
+    var boxSize = width;
+    if (width > 850) {
+      boxSize = width/5;
+    } 
+    
+    var imgNum = Math.floor((Math.random() * 15) + 1);
+
+    box.css({
+      "width": boxSize + "px",
+      "height": boxSize + "px",
+      // "background-image": "url(\"../plc/15.png\")"
+    });
+
+    var evenBox = $(".leaBox:nth-child(2)");
+    evenBox.css({
+      "width": boxSize + "px",
+      "height": boxSize*2 + "px",
+      // "background-image": "url(\"../plc/15.png\")"
+    });
 
 });
